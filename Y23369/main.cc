ed672d3303268a0d534cb24407f961ba42295805
Martin <martinmolinapavon1970@gmial.com>
2026-03-18 13:33:03 +0100

    Ejercicios

---
+++ Y23369/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string fruta;
+    int num;
+
+    map<string, int> mapa;
+
+    while (cin >> fruta >> num) {
+        mapa[fruta] += num;
+    }
+
+    for (auto it = mapa.begin(); it != mapa.end(); it++) {
+        cout << it->first << " " << it->second << endl;
+    }
+
+    return 0;
+}
