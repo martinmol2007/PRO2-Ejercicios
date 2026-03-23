a768b67a7f3dd4f1652c69de2fd78225cc134353
Martin <martinmolinapavon1970@gmial.com>
2026-03-23 16:28:24 +0100

    U20331

---
+++ U20331/main.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+void imprimir (const set<string>& s) {
+    for (auto it2 = s.begin(); it2 != s.end(); it2++) {
+        cout << " " << (*it2);
+    }
+    cout << endl;
+    return;
+}
+
+int main () {
+    string nombre;
+    int fecha;
+    string ciudad;
+
+    map<pair<int, string>, set<string>> mapa;
+
+    while (cin >> nombre >> fecha >> ciudad) {
+        mapa[{fecha, ciudad}].insert(nombre);
+    }
+
+    for (auto it = mapa.begin(); it != mapa.end(); ++it) {
+        if ((*it).second.size() > 1) {
+            cout << it->first.first << " " << it->first.second << ":";
+            imprimir(it->second);
+        }
+    }
+    return 0;
+}
