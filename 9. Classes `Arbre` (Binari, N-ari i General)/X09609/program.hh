5b0cbd8b25c853f29ca5e4645d2bbed865dfed0b
Martin <martinmolinapavon1970@gmail.com>
2026-05-06 19:26:23 +0200

    Cambios

--- 9. Classes `Arbre` (Binari, N-ari i General)/X09609/program.hh
+++ 9. Classes `Arbre` (Binari, N-ari i General)/X09609/program.hh
@@ -1,38 +1,24 @@
 /* Pre: cert */
-/* Post: el resultat es NULL si m es NULL;
-   en cas contrari, el resultat apunta al node arrel 
-   d'una jerarquia de nodes que es una copia de la
-   jerarquia de nodes que te el node apuntat per m com a arrel */
-static node_arbre* copia_node_arbre(node_arbre* m) {
-
-}
-
-
-// Pre: 
-// Post: El resultat.second apunta al node de menos distancia que contiene x que cuelgan de n i el resultat.first es la distancia
-static pair<int, node_arbre*> i_cerca_x (node_arbre* n, const T& x, int dist) {
-    if (n == nullptr) return {-1, nullptr};
+/* Post: el resultat.second apunta al node de menys distancia al node 
+n que conte x del nodes que pengen de n i el resultat.first es aquesta distancia
+*/
+static pair<int, node_arbre*> i_cerca_x(node_arbre* n, const T& x, int dist) {
+    if (n == nullptr) return {1e9, nullptr};
     else if (n->info == x) return {dist, n};
     else {
         pair<int, node_arbre*> i = i_cerca_x(n->segE, x, dist+1);
         pair<int, node_arbre*> d = i_cerca_x(n->segD, x, dist+1);
-        
-        if(d.first != -1 && d.first < i.first) {
-            return d;
-        }
-        else {
-            return i;
-        }
+
+        if (d.first < i.first) return d;
+        else return i;
     }
 }
 
 
 /* Pre: p.i. = A, asub es buit */
-/* Post: si A conte x, asub es el subarbre d'A resultat de la cerca; 
-         si A no conte x, asub es buit 
-*/
+/* Post: si A conte x, asub es el subarbre d'A resultat de la cerca;
+   si A no conte x, asub es buit */
 void sub_arrel(Arbre& asub, const T& x) {
     pair<int, node_arbre*> res = i_cerca_x(primer_node, x, 0);
-    asub.primer_node = copia_node_arbre (res.second);
+    asub.primer_node = copia_node_arbre(res.second);
 }
-
