1f1588e1ee626bedc93f1169a76d10bb351e1763
martinmol2007 <martinmolinapavon1970@gmail.com>
2026-06-08 11:00:49 +0200

    Cambios

---
+++ ExtraFinal/T10651/main.cc
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#if __has_include("program.hh")
+#include "program.hh"
+#else
+#if __has_include("ArbreG.hh")
+#include "ArbreG.hh"
+#endif
+#endif
+
+#include "ArbreG-io.hh"
+
+int main() {
+    string dummy;
+    int ncases;
+    cin >> ncases;
+
+    string empty_line;
+    getline(cin, empty_line);
+    getline(cin, empty_line);
+    
+    for (int i = 0; i < ncases; i++) {
+        ArbreGen<int> arbol = leer_arbol_int();
+        bool num_iguales = arbol.is_binary();
+        cout << (num_iguales ? "si" : "no") << endl;
+    }
+}
